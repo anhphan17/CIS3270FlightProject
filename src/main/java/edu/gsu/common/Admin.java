bdb9240005c3ec842a58288959dfe04705b100d5
Ian McIntosh <ianamcintosh108@gmail.com>
2024-12-04 16:54:29 -0500

    MCINTOSH3

--- src/main/java/edu/gsu/common/Admin.java
+++ src/main/java/edu/gsu/common/Admin.java
@@ -1,73 +1,29 @@
 package edu.gsu.common;
 
-public class Admin extends Customer {
-    private String adminId;
-    private String adminName;
-    private String adminUsername;
-    private String adminPassword;
-    private String addFlight;
-    private String deleteFlight;
-    private String updateFlight;
 
-//    An admin should be able to do all customer activities in addition to be able to add,
-//    update or delete a flight.
+import java.util.ArrayList;
 
-    public Admin() {
+public class Admin extends User {
+    // Constructor
+    public Admin(int userId, String name, String email, String password) {
+        super(userId, name, email, password);
     }
 
-
-    public Admin(String adminId, String adminName,
-                 String adminUsername, String adminPassword) {
-        this.adminId = adminId;
-        this.adminName = adminName;
-        this.adminUsername = adminUsername;
-        this.adminPassword = adminPassword;
+    // Method to add a new flight
+    public void addFlight(Flight flight, ArrayList<Flight> flightList) {
+        flightList.add(flight);
+        System.out.println("Flight added: " + flight);
     }
 
-
-    public void setAdminId(String adminId) {
-        this.adminId = adminId;
+    // Method to remove a flight
+    public void removeFlight(Flight flight, ArrayList<Flight> flightList) {
+        flightList.remove(flight);
+        System.out.println("Flight removed: " + flight);
     }
 
-    public String getAdminId() {
-        return this.adminId;
-    }
-
-    public void setAdminName(String adminName) {
-        this.adminName = adminName;
-    }
-
-    public String getAdminName() {
-        return this.adminName;
-    }
-
-    public void setAdminUsername(String adminUsername) {
-        this.adminUsername = adminUsername;
-    }
-
-    public String getAdminUsername() {
-        return this.adminUsername;
-    }
-
-    public void setAdminPassword(String adminPassword) {
-        this.adminPassword = adminPassword;
-    }
-
-    public String getAdminPassword() {
-        return this.adminPassword;
-    }
-
-    // DOUBLE CHECK THIS only Admin can do these
-    public void addFlight(String flightDetails) {
-        System.out.println("Flight added: " + flightDetails);
-    }
-
-    public void deleteFlight(String flightId) {
-        System.out.println("Flight deleted: " + flightId);
-    }
-
-    public void updateFlight(String flightId, String updatedDetails) {
-        System.out.println("Flight updated: " + flightId + " with details " + updatedDetails);
+    @Override
+    public boolean login(String email, String password) {
+        System.out.println("Admin-specific login logic.");
+        return super.login(email, password);
     }
 }
-
--- src/main/java/edu/gsu/common/User.java
+++ src/main/java/edu/gsu/common/User.java
@@ -3,4 +3,73 @@ package edu.gsu.common;
 public class User {
     //ID, name, login credentials)
     //switch these from customer class^
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+    User(){}
+
+    User(int userId, String name, String username, String password, String email ){
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+
+    }
+
+    // ACCOUNT INFO
+    public void setUserId(int userid){this.userId = userid;}
+    public int getUserId(){return this.userId;}
+
+    public void setFirstName(String name){this.firstName = name;}
+    public String getFirstName(){return this.firstName;}
+    public void setLastName(String name){this.lastName = name;}
+    public String getLastName(){return this.lastName;}
+    public void setFullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+
+    // (Polymorphism can extend this) GO BACK AND LOOK
+    // FOR ADMIN, ADD POSSIBLE ADMINKEY THAT SHOWS TRUE ADMIN LOGIN
+    public boolean login(String email, String password) {
+        return this.email.equals(email) && this.password.equals(password);
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + userId +
+                ", First Name: " + firstName +
+                ", Last Name: " + lastName +
+                ", Full Name: " + getFullName() +
+                ", Email: " + email;
+    }
 }
