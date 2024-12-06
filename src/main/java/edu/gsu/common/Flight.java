ce96dc5d81bc80eacefc9a2ca0272cf2d1a35ea7
Kieumy Dang <snackmikun@gmail.com>
2024-12-06 16:26:44 -0500

    Mikun3.1

--- src/main/java/edu/gsu/common/Flight.java
+++ src/main/java/edu/gsu/common/Flight.java
@@ -92,9 +92,13 @@ public class Flight {
         return this.DepartureCity.equalsIgnoreCase(other.DepartureCity) &&
                 this.DestinationCity.equalsIgnoreCase(other.DestinationCity);
     }
-    public boolean conflictsWith (Flight otherFlight){
+    public boolean conflictsWith (Flight other){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime thisDeparture = LocalDateTime.parse(this.flightDate + " "+this.DepartureTime, formatter);
+        LocalDateTime thisArrival = LocalDateTime.parse(this.flightDate + " " + this.ArrivalTime, formatter);
+        LocalDateTime otherDeparture = LocalDateTime.parse(other.flightDate + " " + other.DepartureTime, formatter);
+        LocalDateTime otherArrival = LocalDateTime.parse(other.flightDate + " " + other.ArrivalTime, formatter);
+    return (thisDeparture.isBefore(otherArrival)&& thisArrival.isAfter(otherDeparture));
     }
 }
 
