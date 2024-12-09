cb7de7716524f71a72d42fff69d77cd2612e6ad6
anhphan <anhnguyenphan03@gmail.com>
2024-12-09 01:06:37 -0500

    Phan2 Commit13

--- src/main/java/edu/gsu/gui/LoginPage.java
+++ src/main/java/edu/gsu/gui/LoginPage.java
@@ -1,72 +1,75 @@
 package edu.gsu.gui;
 
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.fxml.FXML;
-import javafx.scene.control.Menu;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 
 public class LoginPage extends Application {
 
-    @FXML
-    private TextField txtUsername;
-
-    @FXML private TextField txtPassword;
-
-    @FXML
-    private Button btnLoginAction;
-
     @Override
-    /* public void start(Stage primaryStage) {
-        Button btLogin = new Button("Login");
-        Scene scene = new Scene(btLogin, 300, 200);
-        primaryStage.setTitle("LoginPage");
+    public void start(Stage primaryStage) {
+        // Create the root pane
+        AnchorPane root = new AnchorPane();
+        root.setPrefSize(600, 400);
+        root.setStyle("-fx-background-color: #89CFF0;");
+
+        // Create the username text field
+        TextField txtUsername = new TextField();
+        txtUsername.setPromptText("Username");
+        txtUsername.setLayoutX(214);
+        txtUsername.setLayoutY(174);
+        txtUsername.setPrefSize(190, 25);
+
+        // Create the password text field
+        TextField txtPassword = new TextField();
+        txtPassword.setPromptText("Password");
+        txtPassword.setLayoutX(214);
+        txtPassword.setLayoutY(214);
+        txtPassword.setPrefSize(190, 25);
+
+        // Create the login button
+        Button btnLogin = new Button("Login");
+        btnLogin.setFont(Font.font("Serif", 12));
+        btnLogin.setLayoutX(268);
+        btnLogin.setLayoutY(266);
+        btnLogin.setPrefSize(81, 25);
+
+        // Add button click handler
+        btnLogin.setOnAction(e -> {
+            String username = txtUsername.getText();
+            String password = txtPassword.getText();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                System.out.println("Please enter both username and password.");
+            } else {
+                System.out.println("Login button clicked! Username: " + username + ", Password: " + password);
+            }
+        });
+
+        // Create the label for the title
+        Label lblTitle = new Label("Mia Airlines");
+        lblTitle.setFont(Font.font("Serif", 50));
+        lblTitle.setTextFill(javafx.scene.paint.Color.web("#fffb27"));
+        lblTitle.setLayoutX(175);
+        lblTitle.setLayoutY(50);
+        lblTitle.setPrefSize(411, 113);
+
+        // Add all nodes to the root pane
+        root.getChildren().addAll(txtUsername, txtPassword, btnLogin, lblTitle);
+
+        // Set the scene and stage
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Login Page");
         primaryStage.setScene(scene);
         primaryStage.show();
-    } */
-    public void start(Stage primaryStage) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Menu.class.getResource("/edu/gsu/gui/LoginPage2.fxml"));
-            AnchorPane pane = loader.load();
-
-            BorderPane root = new BorderPane();
-            root.setCenter(pane);
-
-            Scene scene = new Scene(root,600,420);
-            primaryStage.setScene(scene);
-            primaryStage.setResizable(false);
-            primaryStage.setTitle("Mia Airlines");
-            primaryStage.show();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-    @FXML
-    private void handleLoginAction(){
-        String username = txtUsername.getText();
-        String password = txtPassword.getText();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            System.out.println("Oops! Look's like you didn't enter both a username and password! Please enter both.");
-        }
-        else {
-            System.out.println("Login button clicked! Username: " + username + ", Password: " + password);
-        }
+    public static void main(String[] args) {
+        launch(args);
     }
-
-        public static void main(String[] args) {
-            launch(args); // Launch the JavaFX application
-        }
-
 }
